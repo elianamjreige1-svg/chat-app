0b8eb0bca4c287fde690f0923a37f88a17aaf24c
elianamjreige1-svg <eliana.m.jreige1@gmail.com>
2026-05-26 23:33:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+   ssl: {
+    rejectUnauthorized: false
+  }
   
 }).promise();
 pool.on('error', (err) => {
