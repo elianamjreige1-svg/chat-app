70cc07ee2e4c37d8e34a403ebc2a3328d5360ae1
elianamjreige1-svg <eliana.m.jreige1@gmail.com>
2026-05-26 23:55:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ io.on("connection", (socket) => {
   let hasUsername = false;
 
   socket.emit("update users", Object.values(users2));
-
+/*
   socket.on("set username", async (newName) => {
   if (!newName) return;
   const name = newName.trim().toLowerCase();
@@ -173,8 +173,8 @@ io.on("connection", (socket) => {
   }
 });
   
- 
-/*
+ */
+
   socket.on("set username", (newName) => {
     if (!newName) return;
     const name = newName.trim().toLowerCase();
@@ -218,7 +218,7 @@ io.on("connection", (socket) => {
       socket.emit("enable chat");
     });
   });
-*/
+
   socket.on("chat message", (msg) => {
     if (!hasUsername) return;
     const user = users2[socket.id];
